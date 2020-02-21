9080235264e53338e0e928f127d2a7843cb41edf
brucemalis <chrishutchinson31@gmail.com>
2020-02-21 12:53:51 -0600

    IPLTESTING
    /save description (without spaces)
    lets you save a location with description
    
    Left moves back in the list of IPLS
    Right moves forward
    Up requests the IPL
    Down Removes the IPL
    
    PGDown will Teleport to the IPL

---
+++ README.md
@@ -0,0 +1,45 @@
+# plouffe_ImapDebug
+Imap finder, Does the job but yea :)
+I know its not optimised, its not supposed to be.
+
+The only reason i made this is because i couldnt figure a way to identify the iMaps so i made this to give me a chance to document things. 
+If you have a better way or some cool native to use feel free to share.
+If you use this code to find any Imaps please share them.
+
+You can set the delay to load the Imap in the Config.
+You can set it to use Keybinds or not in the Config.
+
+Commands are:
+
+/ImapLoadAll
+Wich is gonna start loading all the imaps with the said delay.
+
+/ImapRemoveAll
+Wich is gonna Unload all maps.
+
+/ImapRequest [Imap]
+To load a specifique Imap.
+
+/ImapRemove [Imap]
+To unload a specifique Imap.
+
+/ImapStop 
+Will Hard stop all the loops.
+
+/ImapSave Or /ImapSave Name
+To either Save the current Imap loaded or Save the current Imap With a Name associated to it.
+
+/ImapWait
+To make the loader wait.
+
+Controls = 
+  [Up Arrow] to save the current map.
+  [Down Arrow] to pause the loading.
+
+Features:
+You can see 
+      CurrentImap loaded
+      Second Imap Loaded
+      Third Imap Loaded
+
+The Saved IMaps are shown on your screen.
---
+++ server.lua
@@ -0,0 +1,35 @@
+
+RegisterNetEvent("save")
+AddEventHandler("save", function(coords, heading, ipl, desc)
+    s = tostring(ipl)
+    c = tostring(coords)
+    h = tostring(heading)
+    d = tostring(desc)
+    print("^3[#] Coords: ^1" ..c.. " ^3[#] Heading = ^1" ..h.. "^3[#]IPL = ^1" ..s.. " Description: " .. d)
+    local file = io.open("D:\\redmServer\\server-data\\resources\\[redemrp]\\totalTesting\\imapCoords.txt", "a")
+                    file:write("\nCoords:" ..c.. " Heading = " ..h.. " IPL = " ..s.. " Description: " ..d)
+                    file:close()
+end)
+
+
+RegisterNetEvent("saveClosest")
+AddEventHandler("saveClosest", function(coords, heading, ipl)
+    s = tostring(ipl)
+    c = tostring(coords)
+    h = tostring(heading)
+    print("^3[#] Coords: ^1" ..coords.. " ^3[#] Heading = ^1" ..heading.. "^3[#]IPL = ^1" ..ipl)
+    local file = io.open("D:\\redmServer\\server-data\\resources\\[redemrp]\\totalTesting\\closestImap.txt", "w")
+                    file:write("\nCoords:" ..coords.. " Heading = " ..heading.. " IPL = " ..ipl)
+                    file:close()
+end)
+
+
+-- RegisterNetEvent("peds")
+-- AddEventHandler("peds", function(peds)
+--     s = tostring(peds)
+
+--     print("^3[#] Coords: ^1" ..coords.. " ^3[#] Heading = ^1" ..heading.. "^3[#]IPL = ^1" ..ipl)
+--     local file = io.open("D:\\redmServer\\server-data\\resources\\[redemrp]\\totalTesting\\closestImap.txt", "w")
+--                     file:write("\nCoords:" ..coords.. " Heading = " ..heading.. " IPL = " ..ipl)
+--                     file:close()
+-- end)
